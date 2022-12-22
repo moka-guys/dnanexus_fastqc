f6deb97b1b1d9e36e20fc77debb5e2c7823a1c1d
aledj2 <aledjones@nhs.net>
2022-12-22 15:22:13 +0000

    update to v1.4.0

--- src/code.sh
+++ src/code.sh
@@ -5,27 +5,31 @@
 # and to output each line as it is executed -- useful for debugging
 set -e -x -o pipefail
 
-#
-# Stream input data
-#
-mark-section "streaming input data"
-mkfifo ./"$reads_name"
-dx cat "$reads" > ./"$reads_name" &
-cat_pid=$!
+mkdir -p ~/out/report_html/QC/ ~/out/stats_txt/QC/
+mark-section "download inputs"
+dx-download-all-inputs --parallel
+		
+# The fastqc docker image 
+Docker_file_ID=project-ByfFPz00jy1fk6PjpZ95F27J:file-GKQkG5006xgkK6vgP4X1vY2P
+dx download ${Docker_file_ID}
+Docker_image_file=$(dx describe ${Docker_file_ID} --name)
+Docker_image_name=$(echo $Docker_image_file | sed s'/.tar//' | sed s'/.gz//')
+docker load < /home/dnanexus/"${Docker_image_file}"
+echo "Using fastqc docker image ${Docker_image_name}"
 
 #
 # Set up some options
 #
-mark-section "running FastQC"
-opts=""
+mark-section "building FastQC arguments"
+opts="$extra_options --extract -k $kmer_size "
 if [ "$contaminants_txt" != "" ]; then
-  opts="$opts -c ./in/contaminants_txt/*"
+  opts="$opts -c $contaminants_txt_path"
 fi
 if [ "$adapters_txt" != "" ]; then
-  opts="$opts -a ./in/adapters_txt/*"
+  opts="$opts -a $adapters_txt_path"
 fi
 if [ "$limits_txt" != "" ]; then
-  opts="$opts -l ./in/limits_txt/*"
+  opts="$opts -l $limits_txt_path"
 fi
 if [ "$format" != "auto" ]; then
   opts="$opts -f $format"
@@ -34,20 +38,17 @@ if [ "$nogroup" == "true" ]; then
   opts="$opts --nogroup"
 fi
 
-#
-# Run FastQC
-#
-mkdir results
-/FastQC/fastqc -t `nproc` --extract -k $kmer_size -o results $opts $extra_options ./"$reads_name"
-wait "$cat_pid"
+mark-section "Running FastQC"
+for (( i=0; i<${#reads[@]}; i++ ))
+do 
+  echo ${reads_prefix[i]}
+  docker run -v /home/dnanexus:/home/dnanexus --rm ${Docker_image_name} $opts -o /home/dnanexus ${reads_path[i]}
+  # move outputs used for multiqc (fastqc_data.txt), and the html file to output specific folders, where needed, renaming with sample specific names
+  mv ~/"${reads_prefix[i]}"_fastqc/fastqc_data.txt ~/out/stats_txt/QC/"${reads_prefix[i]}".stats-fastqc.txt
+  mv ~/*fastqc.html ~/out/report_html/QC/
+done
 
-#
-# Upload results
-#
+
 mark-section "uploading results"
-mkdir -p ~/out/report_html/QC/ ~/out/stats_txt/QC/
-mv results/*/fastqc_report.html ~/out/report_html/QC/"$reads_prefix".stats-fastqc.html
-mv results/*/fastqc_data.txt ~/out/stats_txt/QC/"$reads_prefix".stats-fastqc.txt
-
 dx-upload-all-outputs
 mark-success
